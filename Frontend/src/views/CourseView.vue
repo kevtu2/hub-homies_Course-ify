f650181b98ef39f80441974de29b4383857f38eb
Mario8433 <mario84.233@gmail.com>
2025-02-16 14:50:43 -0700

    first remedy attempt. Issues in that script can't read from radio button.

--- Frontend/src/views/CourseView.vue
+++ Frontend/src/views/CourseView.vue
@@ -24,30 +24,30 @@
                     <p class="mt-8 mb-8">{{ question.question }}</p>
                     <Form v-slot="$form" class="flex flex-col gap-4">
                       <div class="flex flex-col gap-2">
-                        <RadioButtonGroup name="quiz" class="flex flex-col gap-4">
+                        <RadioButtonGroup @submit="checkAnswerRoutine(index, qIndex)" name="quiz" class="flex flex-col gap-4">
                           <div class="flex items-center gap-2">
-                            <RadioButton inputId="a" value="a" />
+                            <RadioButton inputId="a" v-model="selectedAnswer" value="1" :invalid="answerCorrectness"/>
                             <label for="a">{{ question.a }}</label>
                           </div>
                           <div class="flex items-center gap-2">
-                            <RadioButton inputId="b" value="b" />
+                            <RadioButton inputId="b" v-model="selectedAnswer" value="2" :invalid="!answerCorrectness"/>
                             <label for="b">{{ question.b }}</label>
                           </div>
                           <div class="flex items-center gap-2">
-                            <RadioButton inputId="c" value="c" />
+                            <RadioButton inputId="c" v-model="selectedAnswer" value="3" :invalid="!answerCorrectness"/>
                             <label for="c">{{ question.c }}</label>
                           </div>
                           <div class="flex items-center gap-2">
-                            <RadioButton inputId="d" value="d" />
+                            <RadioButton inputId="d" v-model="selectedAnswer" value="4" :invalid="!answerCorrectness"/>
                             <label for="d">{{ question.d }}</label>
                           </div>
                         </RadioButtonGroup>
                       </div>
-                      <Button v-if="!visibleAnswers[index][qIndex]" class="w-40" type="button" severity="secondary" label="Show the answer" @click="toggleVisibility(index,qIndex)" />
-                      <Button v-if="visibleAnswers[index][qIndex]" class="w-40" type="button" severity="secondary" label="Hide the answer" @click="toggleVisibility(index,qIndex)" />
+                      <Button v-if="!visibleAnswers[index][qIndex]" class="w-40" type="submit" severity="secondary" label="Submit" @click="checkAnswerRoutine(index,qIndex)" />
+                      <Button v-if="visibleAnswers[index][qIndex]" class="w-40" type="button" severity="secondary" label="Hide the answer" @click="clearAnswerRoutine(index,qIndex)" />
                     </Form>
                     <p v-if="visibleAnswers[index][qIndex]" class="text-xl mt-5 mb-5">Reference Answer</p>
-                    <p v-if="visibleAnswers[index][qIndex]">{{ question.answer }}</p>
+                    <p v-if="visibleAnswers[index][qIndex]">{{ mapAnswer[question.answer] }}</p>
                   </AccordionContent>
                 </AccordionPanel>
               </Accordion>
@@ -94,7 +94,7 @@ const courseData = ref({
       "questions": [
         {
           "question": "QUESTION",
-          "answer": "ANSWER",
+          "answer": "4",
           "a": "OPTION1",
           "b": "OPTION2",
           "c": "OPTION3",
@@ -102,7 +102,7 @@ const courseData = ref({
         },
         {
           "question": "QUESTION",
-          "answer": "ANSWER",
+          "answer": "1",
           "a": "OPTION1",
           "b": "OPTION2",
           "c": "OPTION3",
@@ -116,7 +116,7 @@ const courseData = ref({
       "questions": [
         {
           "question": "QUESTION",
-          "answer": "ANSWER",
+          "answer": "2",
           "a": "OPTION1",
           "b": "OPTION2",
           "c": "OPTION3",
@@ -124,7 +124,7 @@ const courseData = ref({
         },
         {
           "question": "QUESTION",
-          "answer": "ANSWER",
+          "answer": "3",
           "a": "OPTION1",
           "b": "OPTION2",
           "c": "OPTION3",
@@ -135,11 +135,48 @@ const courseData = ref({
   ]
 })
 
+const mapAnswer: {
+  [key: string]: string;
+} = {
+  '1': 'A',
+  '2': 'B',
+  '3': 'C',
+  '4': 'D'
+}
+
 const visibleAnswers = ref(courseData.value.sections.map(section => section.questions.map(() => false)));
+const answerCorrectness = ref(true);
+const selectedAnswer = ref('');
+
+const checkAnswerRoutine = (index: number, qIndex: number) => {
+      const realAnswer = courseData.value.sections[index].questions[qIndex].answer;
+      if (!Object.keys(mapAnswer).includes(realAnswer)) {
+        alert('BUG: There is no valid answer for this question');
+        console.log("proposed invalid answer: ",realAnswer)
+        return;
+      }
+      if (selectedAnswer.value !== null) {
+        if (selectedAnswer.value == realAnswer) {
+          answerCorrectness.value = true;
+        } else {
+          answerCorrectness.value = false;
+        }
+        console.log("Answer correctness determination: ",answerCorrectness.value,"Real answer: ", realAnswer, ", selected answer: ",selectedAnswer.value);
+      } else {
+        alert('Please select an option!');
+        console.log("The selectedAnswer variable answer: ",selectedAnswer.value)
+      }
+      toggleVisibility(index,qIndex);
+    };
+
+const clearAnswerRoutine = (index: number, qIndex: number) => {
+  const answerCorrectness = ref(false);
+  const selectedAnswer = ref('');
+  toggleVisibility(index,qIndex);
+}
 
 const toggleVisibility = async (index: number,qIndex: number) => {
   visibleAnswers.value[index][qIndex] = !visibleAnswers.value[index][qIndex];
-  console.log(visibleAnswers.value[0]);
 }
 
 axios.get('localhost:3000/api/courses/:c_id')
